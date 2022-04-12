1396bd28900384d860bf43c2bc87f244863188f1
B <77552838+Mrballer59@users.noreply.github.com>
2022-04-12 19:21:58 +0100

    JQ animation sections

--- JS/Test.js
+++ JS/Test.js
@@ -11,11 +11,26 @@ $(document).ready(function(){
 
 }); 
 
-// new bit 
-$(document).ready(function(){
-    $(".about-section").waypoint(function(direction){
-       $()
-    })
-
+// new bit animation on scoll
+$(".js--about-section").waypoint(function(direction){
+    $(".js--about-box").addClass("animate__animated animate__fadeIn");
+   
+}, {
+    offset:"50%"
 }); 
+
+$(".js--services-section").waypoint(function(direction){
+    $(".js--service-box").addClass("animate__animated animate__zoomIn");
+   
+}, {
+    offset:"50%"
+}); 
+
+$(".js--packages-section").waypoint(function(direction){
+    $(".js--enterprise").addClass("animate__animated animate__pulse");
+   
+});
+
+
+
 //JQ is a bit out of date
